42ec5277c28618e9f0ee2a3acb60f565d87cad50
hoffm <m@hof.fm>
2012-12-12 16:29:14 -0500

    Revision of Q4.

--- q4.rb
+++ q4.rb
@@ -1,19 +1,13 @@
-def palandrome(int)
-  str = int.to_s
-  len = str.length
-  segcond_seg_start = len.odd? ? len/2 + 1 : len/2
-  str[0..(len/2 - 1)] == str[(len/2)..-1].reverse
-end
-
-def answers
+def answer
   a = (100..999).to_a.reverse
   b = a
   ans = []
   a.each do |n|
     b.each do |m|
-      ans << [[n,m], n*m] if palandrome(n*m)
+      palandrome = (n*m).to_s == (n*m).to_s.reverse
+      ans << n*m if palandrome
     end
   end
 
-  ans.sort_by{|el| el[1]}.last.last
+  ans.sort.last
 end
